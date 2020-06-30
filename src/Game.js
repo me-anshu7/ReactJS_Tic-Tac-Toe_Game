db7d5f02d0c7078479884b4e3399aa53aee53f78
me-anshu7 <63518358+me-anshu7@users.noreply.github.com>
2020-06-30 18:53:50 +0530

    Passing data through props and making interactive componenets

--- src/Game.js
+++ src/Game.js
@@ -11,14 +11,25 @@ import "./styles.css";
 } */
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
-    return <button className="square">{/*To-Do*/}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
